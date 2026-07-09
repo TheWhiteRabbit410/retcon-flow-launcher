7ab4980271f0fcf53828ea8d6bca2494eb43eb8f
TheWhiteRabbit410 <coolisutherland@gmail.com>
2026-07-10 00:32:53 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,44 @@
-self.addEventListener("install", function() {
+const CACHE_NAME = "retcon-flow-launcher-v1";
+
+const CORE_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./config.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CORE_FILES);
+    })
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", function(event) {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
